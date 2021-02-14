ff2bebbf768e6649a3d3e6fe2a234b8ec557e5ef
meeeeju <68631158+meeeeju@users.noreply.github.com>
2021-02-14 22:24:13 +0900

    Update 11053.py
    이해가 안되서 손코딩하니까 이해가 되었다.
    근데 재귀함수 써서 시간 초과 떳다 근데 12시 될까봐 미리 올림요 
    내가 위에 쓴 방법은 완전탐색 알고리즘인데, 인터넷에서 거의 뺏기고 이해를 해보았다..
    호성이가 말한 dynamic algorithm을 사용해야 할듯..

--- BOJ/[11053] 가장 긴 증가하는 부분 수열/Meeju/11053.py
+++ BOJ/[11053] 가장 긴 증가하는 부분 수열/Meeju/11053.py
@@ -1,1 +1,21 @@
-미주 코드 넣으삼ㅎㅎ
+import sys
+
+def lis(arr):
+    if not arr:
+        return 0
+    ret=1
+    for i in range(len(arr)):               #이중 for문 첫번째 for문은 i가 수열의 첫번째라고 생각하고 시작하기 위함 
+                                            #두번째 for문은 i가 첫번재인 for문에 대해서 i보다 큰 숫자를 nxt리스트에 저장해서 최대 ret를 구하기 위함
+        nxt=[]
+        for j in range(i+1,len(arr)):
+            if arr[i] <arr[j]:
+                nxt.append(arr[j])
+        ret=max(ret,1+lis(nxt))             
+    return ret
+
+def main():
+    count = sys.stdin.readline()     
+    num_list = sys.stdin.readline().split()
+    num_list = list(map(int, num_list))       #입력값 한줄로 받아들여서 정수로 변환후 list로 저장
+    print(lis(num_list))                      #lis 함수 호출  -> 반환값 가장 긴 길이
+main()
