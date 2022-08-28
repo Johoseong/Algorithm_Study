ca2086bcf77754dc6f682926cfa77f347cbe22f9
Jo HoSeong <62900140+Johoseong@users.noreply.github.com>
2022-08-28 17:57:08 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 229.75 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,32 +1,25 @@
+def palindrome(s, N, l, r):
+    cnt = 0
+    while True:
+        if l < 0 or r >= N: break
+        
+        if s[l] == s[r]:
+            cnt += 2
+            l -= 1
+            r += 1
+        else: break
+    return cnt
+
 def solution(s):
     answer = 1
     N = len(s)
 
     for center in range(1, N - 1):
-        l, r = center - 1, center + 1
-        cnt = 0
-        while True:
-            if l < 0 or r >= N: break
-
-            if s[l] == s[r]:
-                cnt += 2
-                l -= 1
-                r += 1
-            else: break
-        if cnt != 0: cnt += 1
+        cnt = palindrome(s, N, center - 1, center + 1) + 1
         answer = max(answer, cnt)
 
     for center in range(1, N):
-        l, r = center - 1, center
-        cnt = 0
-        while True:
-            if l < 0 or r >= N: break
-
-            if s[l] == s[r]:
-                cnt += 2
-                l -= 1
-                r += 1
-            else: break
+        cnt = palindrome(s, N, center - 1, center)
         answer = max(answer, cnt)
 
     return answer
