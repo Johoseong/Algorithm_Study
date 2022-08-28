7e9d5728e848cbf765006e42e530405e82468795
Jo HoSeong <62900140+Johoseong@users.noreply.github.com>
2022-08-28 18:55:11 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 3560.22 ms, Memory: 398 MB -BaekjoonHub

--- 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -1,3 +1,41 @@
-from math import factorial as f
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+def dfs(cur, left_cnt, right_cnt, n):
+    global answer
+
+    if left_cnt == n and right_cnt == n:
+        answer.add(cur)
+        return
+
+    if left_cnt < n:
+        dfs(cur + "(", left_cnt + 1, right_cnt, n)
+        if left_cnt > right_cnt:
+            dfs(cur + ")", left_cnt, right_cnt + 1, n)
+    else:
+        dfs(cur + ")", left_cnt, right_cnt + 1, n)
+
 def solution(n):
-    return f(2*n)//(f(n)**2*(n+1))
+    global answer
+    answer = set()
+
+    dfs("(", 1, 0, n)
+    return len(answer)
